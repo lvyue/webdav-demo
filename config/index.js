a3e742da8c3beb39094cb790d703ca6cf51e74ff
Yue Lv <lvyuely@yeah.net>
2018-02-28 21:33:42 +0800

    添加config 配置

---
+++ config/index.js
@@ -0,0 +1,11 @@
+let config
+if (process.env.NODE_ENV === 'production') {
+    config = require('./prod');
+} else if (process.env.NODE_ENV === 'test') {
+    config = require('./test');
+}else {
+    config = require('./dev');
+}
+
+
+module.exports = config;
